7730a98c3d1a2946d7c84251065f97b73eb024f2
hunte <besscomdark@gmail.com>
2024-07-06 19:16:41 +0800

    調整

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 //     var wrapperStart = document.querySelector('.wrapper').offsetTop;
 //     var wrapperHeight = document.querySelector('.wrapper').offsetHeight;
 //     var startScroll = 150; // 將觸發點設定為 150px
-    
+
 
 //     // 只在滾動位置進入 .wrapper 區域時執行動畫
 //     if (scrollPosition >= wrapperStart && scrollPosition <= (wrapperStart + wrapperHeight)) {
@@ -19,7 +19,7 @@
 //     }
 // });
 
-document.addEventListener('scroll', function() {
+document.addEventListener('scroll', function () {
     var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
     var wrapper = document.querySelector('.wrapper');
     var wrapperStart = wrapper.offsetTop;
@@ -32,18 +32,18 @@ document.addEventListener('scroll', function() {
         var moveX = (relativeScroll / (wrapperHeight - window.innerHeight)) * totalWidth;
 
         // 根據滾動比例移動圖片
-        gsap.to('.spot_bg_show', {x: -moveX, ease: "none"});
-        
+        gsap.to('.spot_bg_show', { x: -moveX, ease: "none" });
+
         // 阻止頁面上下滾動
         document.body.style.overflow = 'hidden';
     } else {
         // 恢復頁面上下滾動
         document.body.style.overflow = 'auto';
-        
+
         if (scrollPosition < wrapperStart) {
-            gsap.to('.spot_bg_show', {x: 0, ease: "none"});
+            gsap.to('.spot_bg_show', { x: 0, ease: "none" });
         } else if (scrollPosition > (wrapperStart + wrapperHeight - window.innerHeight)) {
-            gsap.to('.spot_bg_show', {x: -totalWidth, ease: "none"});
+            gsap.to('.spot_bg_show', { x: -totalWidth, ease: "none" });
         }
     }
 });
@@ -52,7 +52,7 @@ document.addEventListener('scroll', function() {
 
 
 // recommend區
-$(document).ready(function() {
+$(document).ready(function () {
     var $slider = $('.recommend_slider');
     $slider.slick({
         infinite: true,
@@ -69,7 +69,7 @@ $(document).ready(function() {
 });
 
 // intelligence區
-$(document).ready(function() {
+$(document).ready(function () {
     var $slider = $('.info_post');
     $slider.slick({
         infinite: true,
@@ -85,14 +85,16 @@ $(document).ready(function() {
 });
 
 // about頁面換圖效果
-function init(e) {
-    if (!e.target.closest('.about_item')) return;
-    let hero = document.querySelector('div[data-pos="0"]');
-    let target = e.target.parentElement;
-    [target.dataset.pos,hero.dataset.pos] = [hero.dataset.pos,target.dataset.pos];
-  }
-  
-  window.addEventListener('click',init,false);
+$('.gallery_about_img').slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    pauseOnHover: true,
+    arrows: false
+});
 
 // 捲動效果
 $(function () {
@@ -123,11 +125,11 @@ $('.service_img').slick({
     adaptiveHeight: true,
     pauseOnHover: true,
     arrows: false
-  });
+});
 
 
 // 選單效果
 $(".menu_btn").click(function () {
     $(".menu_wrap").toggleClass("active");
     $(this).toggleClass("active");
-  });
+});
